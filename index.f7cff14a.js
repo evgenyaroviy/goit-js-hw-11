5fa84d66adeabadca3f60d518373ea1f29ea4ba3
evgenyaroviy <120398316+evgenyaroviy@users.noreply.github.com>
2023-05-23 15:05:43 +0000

    Deploying to gh-pages from @ evgenyaroviy/goit-js-hw-11@eab90acfa11a64837c112c4602c3adf7df48426b 🚀

---
+++ index.f7cff14a.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector("form"),input:document.querySelector("input"),button:document.querySelector("button"),gallery:document.querySelector(".gallery")};e.form.addEventListener("submit",(function(t){t.preventDefault();var r=e.input.value;return console.log(r),r}))}();
+//# sourceMappingURL=index.f7cff14a.js.map
