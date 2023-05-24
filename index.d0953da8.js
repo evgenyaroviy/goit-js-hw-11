88bb6f315a84df23ff877034da424a1528b85c7f
evgenyaroviy <120398316+evgenyaroviy@users.noreply.github.com>
2023-05-24 16:22:39 +0000

    Deploying to gh-pages from @ evgenyaroviy/goit-js-hw-11@cd37f98414cb9355622d255843df33a951c72864 🚀

--- index.d0953da8.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector("form"),input:document.querySelector("input"),button:document.querySelector("button"),gallery:document.querySelector(".gallery")};e.form.addEventListener("submit",(function(t){t.preventDefault();const o=e.input.value;return console.log(o),o}));
-//# sourceMappingURL=index.d0953da8.js.map
