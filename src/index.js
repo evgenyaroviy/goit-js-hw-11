4a5e31fc07c6f4dbe20197d742dae5d4a8e7208c
kaktuz <120398316+evgenyaroviy@users.noreply.github.com>
2023-05-25 02:57:00 +0300

    update hw11

--- src/index.js
+++ src/index.js
@@ -1,56 +1,104 @@
 import Notiflix from 'notiflix';
+import axios from 'axios';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '36658431-70b367d2ef8581f2f498a1946';
 const parameters = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40'
+let page;
 const refs = {
-    form: document.querySelector('form'),
-    input: document.querySelector('input'),
-    button: document.querySelector('button'),
-    gallery: document.querySelector('.gallery')
+  form: document.querySelector('form'),
+  input: document.querySelector('input'),
+  button: document.querySelector('button'),
+  gallery: document.querySelector('.gallery')
 }
 
 refs.form.addEventListener('submit', onInput)
 
 
 function onInput(e) {
-    e.preventDefault();
-    const params = refs.input.value;
-    requestToBeckend(params)
-    console.log(params);
-    // return params;
+  e.preventDefault();
+  const params = refs.input.value;
+  requestToBeckend(params)
 }
 
-function requestToBeckend(params) {
-    return fetch(`${BASE_URL}?key=${API_KEY}&q=${params}&${parameters}`)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(response.statusText)
-      } 
-      return response.json();
-    })
-      .then(searchError)
-      .then(addCard)
-    // .catch((error) => {
-    //     Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-    //   });
+function clearList() {
+  refs.gallery.innerHTML = '';
 }
 
-function searchError(response) {
-  const { total, totalHits, hits } = response;
-  console.log(hits.length)
-    if (Number(hits.length) === 0) {
-        throw new Error('No results found');
-      }
-      return { total, totalHits, hits };
-    }
 
-    function createCard(hits) {
-  return hits.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+
+async function requestToBeckend(params) {
+  clearList()
+  try {
+    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${params}&${parameters}&${page}`)
+    const dataResp = response.data;
+    const hitDataResp = response.data.hits;
+    console.log(dataResp)
+    console.log(hitDataResp)
+
+    await searchError(dataResp);
+    await addCard(hitDataResp);
+
+  } catch (error) {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  };
+}
+
+function searchError(dataResp) {
+  const { total, totalHits, hits } = dataResp;
+  if (Number(hits.length) === 0) {
+    throw new Error('No results found');
+  }
+  Notiflix.Notify.success(`Hooray! We found ${dataResp.totalHits} images.`);
+  return { total, totalHits, hits };
+}
+
+// let options = {
+//   root: null,
+//   rootMargin: "400px",
+//   threshold: 0,
+// };
+// let observer = new IntersectionObserver(handlerPagination, options);
+
+// const guard = document.createElement('div');
+// guard.id = 'sentinel';
+// refs.gallery.append(guard);
+
+// observer.observe(guard);
+
+// function handlerPagination(entries, observer) {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       page += 1;
+//       console.log(refs.input.value)
+//       requestToBeckend(refs.input.value)
+//         .then(({ hitDataResp }) => {
+//           refs.gallery.insertAdjacentHTML('beforeend', createCard(hitDataResp));
+//           let gallery = new SimpleLightbox('.gallery a');
+
+//           if (page >= Math.ceil(data.totalHits / 40)) {
+//             observer.unobserve(guard);
+//           }
+//         })
+//     }
+//   })
+// }
+
+
+function createCard(hitDataResp) {
+  console.log(hitDataResp)
+  return hitDataResp.map(({ webformatURL, tags, likes, views, comments, downloads, largeImageURL }) => {
     return `
     <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <a href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="400px" height="250px" />
+  </a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
@@ -74,6 +122,17 @@ function searchError(response) {
   }).join('');
 }
 
-function addCard(data) {
-  return refs.gallery.insertAdjacentHTML('beforeend', createCard(data));
+function addCard(hitDataResp) {
+  refs.gallery.insertAdjacentHTML('beforeend', createCard(hitDataResp));
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('show.simplelightbox', function addCard() {
+    const { height: cardHeight } = document
+      .querySelector(".gallery")
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: "smooth",
+    });
+  });
 }
